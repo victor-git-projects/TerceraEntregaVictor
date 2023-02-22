4e8f956e137f1cfb42cd2c326c48e243911451bd
Victor Davila <victordavila04@gmail.com>
2023-02-21 22:49:44 -0500

    Funciona las paginas web a traves de appcoder

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -5,19 +5,24 @@ from AppCoder.models import Curso
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("vista inicio")
+    return render(request, 'AppCoder/inicio.html')
+    #return HttpResponse("vista inicio")
 
 def cursos(request):
-    return HttpResponse("vista cursos")
+    return render(request, 'AppCoder/cursos.html')
+    #return HttpResponse("vista cursos")
 
 def profesores(request):
-    return HttpResponse("vista profesores")
+    return render(request, 'AppCoder/profesores.html')
+    #return HttpResponse("vista profesores")
 
 def entregables(request):
-    return HttpResponse("vista entregables")
+    return render(request, 'AppCoder/entregables.html')
+    #return HttpResponse("vista entregables")
 
 def estudiantes(request):
-    return HttpResponse("vista estudiantes")
+    return render(request, 'AppCoder/estudiantes.html')
+    #return HttpResponse("vista estudiantes")
 
 
 """
