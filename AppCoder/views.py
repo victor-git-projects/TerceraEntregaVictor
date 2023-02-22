7a196d92844a5660ec6f83c0ecbb638af3811054
Victor Davila <victordavila04@gmail.com>
2023-02-21 22:31:37 -0500

    Se crearon solo los models y el url dentro de appcoder

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -4,8 +4,29 @@ from AppCoder.models import Curso
 
 # Create your views here.
 
+def inicio(request):
+    return HttpResponse("vista inicio")
+
+def cursos(request):
+    return HttpResponse("vista cursos")
+
+def profesores(request):
+    return HttpResponse("vista profesores")
+
+def entregables(request):
+    return HttpResponse("vista entregables")
+
+def estudiantes(request):
+    return HttpResponse("vista estudiantes")
+
+
+"""
+Esto solo fue una prueba, ya no es necesario par ael proyecto
+
 def curso(self):
     curso = Curso(nombre="Desarrollo web", comision=19881)
     curso.save()
     documentoDeTexto = f'--> Curso:{curso.nombre} comision:{curso.comision}'
     return HttpResponse(documentoDeTexto)
+
+"""
